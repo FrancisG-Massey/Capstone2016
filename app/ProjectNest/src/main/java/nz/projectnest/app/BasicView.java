dddc6ad991fde633d9efcd1f3cc7ee7b52f980f2
FrancisG-Massey <francisg.massey@gmail.com>
2016-07-24 19:04:03 +1200

    Created app structure

---
+++ app/ProjectNest/src/main/java/nz/projectnest/app/BasicView.java
@@ -0,0 +1,36 @@
+package nz.projectnest.app;
+
+import com.gluonhq.charm.glisten.control.AppBar;
+import com.gluonhq.charm.glisten.control.Icon;
+import com.gluonhq.charm.glisten.mvc.View;
+import com.gluonhq.charm.glisten.visual.MaterialDesignIcon;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+
+public class BasicView extends View {
+
+    public BasicView(String name) {
+        super(name);
+        
+        Label label = new Label("Hello JavaFX World!");
+
+        Button button = new Button("Change the World!");
+        button.setGraphic(new Icon(MaterialDesignIcon.LANGUAGE));
+        button.setOnAction(e -> label.setText("Hello JavaFX Universe!"));
+        
+        VBox controls = new VBox(15.0, label, button);
+        controls.setAlignment(Pos.CENTER);
+        
+        setCenter(controls);
+    }
+
+    @Override
+    protected void updateAppBar(AppBar appBar) {
+        appBar.setNavIcon(MaterialDesignIcon.MENU.button(e -> System.out.println("Menu")));
+        appBar.setTitleText("Basic View");
+        appBar.getActionItems().add(MaterialDesignIcon.SEARCH.button(e -> System.out.println("Search")));
+    }
+    
+}
---
+++ app/ProjectNest/src/main/java/nz/projectnest/app/NestApplication.java
@@ -0,0 +1,26 @@
+package nz.projectnest.app;
+
+import com.gluonhq.charm.glisten.application.MobileApplication;
+import com.gluonhq.charm.glisten.visual.Swatch;
+import javafx.scene.Scene;
+
+import javafx.stage.Stage;
+import com.gluonhq.charm.glisten.license.License;
+
+@License(key="482637c8-d766-40fa-942e-f96a11d31da8")
+public class NestApplication extends MobileApplication {
+
+    public static final String BASIC_VIEW = HOME_VIEW;
+
+    @Override
+    public void init() {
+        addViewFactory(BASIC_VIEW, () -> new BasicView(BASIC_VIEW));
+    }
+
+    @Override
+    public void postInit(Scene scene) {
+        Swatch.BLUE.assignTo(scene);
+
+
+    }
+}
