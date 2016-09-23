61bdbdb90dcd09614b08ddefaf564b6f10574039
Sam Hunt <s2112201@ipc.ac.nz>
2016-09-24 02:27:42 +1200

    Issue #41 Numeric typed db fields now return with correct format in JSON

--- ProjectNest-API/src/java/org/nestnz/api/Common.java
+++ ProjectNest-API/src/java/org/nestnz/api/Common.java
@@ -86,6 +86,9 @@ public class Common {
                     case java.sql.Types.BIT:
                         jsonObj.addProperty(column_name, rsh.getBoolean(i));
                         break;
+                    case java.sql.Types.NUMERIC:
+                    case java.sql.Types.DECIMAL:
+                        jsonObj.addProperty(column_name, rsh.getBigDecimal(i));
                     case java.sql.Types.DOUBLE:
                         jsonObj.addProperty(column_name, rsh.getDouble(i));
                         break;
