77cb37a037af4d151fe7f7dc14ffbe244084d2d0
MJ LEE <itmjlee@gmail.com>
2016-09-27 17:46:04 +1300

    Removed unused lines

--- ProjectNest-Web/html/js/app.js
+++ ProjectNest-Web/html/js/app.js
@@ -62,42 +62,6 @@ var myApp = angular
 			redirectTo : '/'
 		});
 	} ])
-/*>>>>>>> Stashed changes
-
-			.when('/', {
-				controller : 'HomeController',
-				templateUrl : 'templates/home.html'
-			})
-
-			.when('/about', {
-				controller : 'HomeController',
-				templateUrl : 'templates/about.html'
-			}).when('/contact', {
-				controller : 'HomeController',
-				templateUrl : 'templates/contact.html'
-			}).when('/statistics', {
-				controller : 'HomeController',
-				templateUrl : 'templates/statistics.html'
-			}).when('/volunteer', {
-				controller : 'HomeController',
-				templateUrl : 'templates/volunteer.html'
-			}).when('/trapline-admin', {
-				controller : 'AdminTraplineController',
-				templateUrl : 'modules/admin/views/trapline-admin.html'
-			}).when('/trap-admin', {
-				controller : 'AdminTrapController',
-				templateUrl : 'modules/admin/views/trap-admin.html'
-			}).when('/volunteer-admin/:traplineId', {
-				controller : 'AdminVolunteerController',
-				templateUrl : 'modules/admin/views/volunteer-admin.html'
-			}).when('/trap-admin/:traplineId', {
-				controller : 'AdminTrapController',
-				templateUrl : 'modules/admin/views/trap-admin.html'
-			}).otherwise({
-				redirectTo : '/'
-			});
-		} ])*/
-
 		.run(
 				[
 						'$rootScope',
