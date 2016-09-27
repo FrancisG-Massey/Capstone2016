fe55553531c5a2f9afe29221562c2d28c6c5fe2b
MJ LEE <itmjlee@gmail.com>
2016-09-27 15:43:58 +1300

    #61 issue resolved
    1. Fixed placement of save button. 
    2. Removed repeat Add Trap Line button.
    3. Remove table headers.
    4. Changed button content.

--- ProjectNest-Web/html/js/app.js
+++ ProjectNest-Web/html/js/app.js
@@ -4,105 +4,137 @@
 angular.module('Authentication', []);
 angular.module('Admin', []);
 
-var myApp = angular.module('myApp', [
-    'Authentication',
-    'Admin',
-    'ngRoute',
-    'ngCookies'
-])
- 
-.config(['$httpProvider', function($httpProvider) {
-        $httpProvider.defaults.useXDomain = true;
-        delete $httpProvider.defaults.headers.common['X-Requested-With'];
-    }
-])
-.config(['$routeProvider', function ($routeProvider) {
+var myApp = angular
+		.module('myApp', [ 'Authentication', 'Admin', 'ngRoute', 'ngCookies' ])
 
-    $routeProvider
-        .when('/login', {
-            controller: 'LoginController',
-            templateUrl: 'modules/authentication/views/login.html',
-        })
- 
-        .when('/', {
-            controller: 'HomeController',
-            templateUrl: 'templates/home.html'
-        })
+		.config([ '$httpProvider', function($httpProvider) {
+			$httpProvider.defaults.useXDomain = true;
+			delete $httpProvider.defaults.headers.common['X-Requested-With'];
+		} ])
+		.config([ '$routeProvider', function($routeProvider) {
 
-        .when('/about', {
-            controller: 'HomeController',
-            templateUrl: 'templates/about.html'
-        })
-        .when('/contact', {
-            controller: 'HomeController',
-            templateUrl: 'templates/contact.html'
-        })
-        .when('/statistics', {
-            controller: 'HomeController',
-            templateUrl: 'templates/statistics.html'
-        })
-        .when('/volunteer', {
-            controller: 'HomeController',
-            templateUrl: 'templates/volunteer.html'
-        })
-        .when('/trapline-admin',{
-            controller: 'AdminTraplineController',
-            templateUrl: 'modules/admin/views/trapline-admin.html'        	
-        })
-        .when('/trap-admin', {
-            controller: 'AdminTrapController',
-            templateUrl: 'modules/admin/views/trap-admin.html'
-        })
-        .when('/volunteer-admin/:traplineId', {
-            controller: 'AdminVolunteerController',
-            templateUrl: 'modules/admin/views/volunteer-admin.html'
-        })
-        .when('/trap-admin/:traplineId', {
-            controller: 'AdminTrapController',
-            templateUrl: 'modules/admin/views/trap-admin.html'
-        })
-        .otherwise({ redirectTo: '/' });
-}])
+			$routeProvider.when('/login', {
+				controller : 'LoginController',
+				templateUrl : 'modules/authentication/views/login.html',
+			})
 
-.run(['$rootScope', '$location', '$cookieStore', '$http', function ($rootScope, $location, $cookieStore, $http) {
-    // keep user logged in after page refresh
-    $('#navbar').collapse('hide');
-	
-    $rootScope.globals = $cookieStore.get('globals') || {};
-    console.log($rootScope.globals.currentUser);
-    if ($rootScope.globals.currentUser) {
-        $http.defaults.headers.common['Session-Token'] = $rootScope.globals.currentUser.sessionToken; // jshint ignore:line
+			.when('/', {
+				controller : 'HomeController',
+				templateUrl : 'templates/home.html'
+			})
 
-    }
+			.when('/about', {
+				controller : 'HomeController',
+				templateUrl : 'templates/about.html'
+			}).when('/contact', {
+				controller : 'HomeController',
+				templateUrl : 'templates/contact.html'
+			}).when('/statistics', {
+				controller : 'HomeController',
+				templateUrl : 'templates/statistics.html'
+			}).when('/volunteer', {
+				controller : 'HomeController',
+				templateUrl : 'templates/volunteer.html'
+			}).when('/trapline-admin', {
+				controller : 'AdminTraplineController',
+				templateUrl : 'modules/admin/views/trapline-admin.html'
+			}).when('/trap-admin', {
+				controller : 'AdminTrapController',
+				templateUrl : 'modules/admin/views/trap-admin.html'
+			}).when('/volunteer-admin/:traplineId', {
+				controller : 'AdminVolunteerController',
+				templateUrl : 'modules/admin/views/volunteer-admin.html'
+			}).when('/trap-admin/:traplineId', {
+				controller : 'AdminTrapController',
+				templateUrl : 'modules/admin/views/trap-admin.html'
+			}).otherwise({
+				redirectTo : '/'
+			});
+		} ])
 
+		.run(
+				[
+						'$rootScope',
+						'$location',
+						'$cookieStore',
+						'$http',
+						function($rootScope, $location, $cookieStore, $http) {
+							// keep user logged in after page refresh
+							$('#navbar').collapse('hide');
 
-    $rootScope.$on('$locationChangeStart', function (event, next, current) {
-        // redirect to login page if not logged in
-        if ($location.path() !== '/login' && !$rootScope.globals.currentUser) {
-             //$location.path('/login');
-        }
-    });
-}])
+							$rootScope.globals = $cookieStore.get('globals')
+									|| {};
+							console.log($rootScope.globals.currentUser);
+							if ($rootScope.globals.currentUser) {
+								$http.defaults.headers.common['Session-Token'] = $rootScope.globals.currentUser.sessionToken; // jshint
+																																// ignore:line
 
-.directive('bsActiveLink', ['$location', function ($location) {
-    return {
-        restrict: 'A', //use as attribute 
-        replace: false,
-        link: function (scope, elem) {
-            //after the route has changed
-            scope.$on("$routeChangeSuccess", function () {
-                var hrefs = ['/#' + $location.path(),
-                             '#' + $location.path(), //html5: false
-                             $location.path()]; //html5: true
-                angular.forEach(elem.find('a'), function (a) {
-                    a = angular.element(a);
-                    if (-1 !== hrefs.indexOf(a.attr('href'))) {
-                        a.parent().addClass('active');
-                    } else {
-                        a.parent().removeClass('active');   
-                    };
-                });     
-            });
-        }
-    }
-}]);
+							}
+
+							$rootScope.$on('$locationChangeStart', function(
+									event, next, current) {
+								// redirect to login page if not logged in
+								if ($location.path() !== '/login'
+										&& !$rootScope.globals.currentUser) {
+									// $location.path('/login');
+								}
+							});
+						} ])
+
+		/*
+		 * .controller('BackButton',['$scope',function ($scope) { $scope.goBack =
+		 * function() { window.history.back(); }; }])
+		 */
+
+		.directive(
+				'bsActiveLink',
+				[
+						'$location',
+						function($location) {
+							return {
+								restrict : 'A', // use as attribute
+								replace : false,
+								link : function(scope, elem) {
+									// after the route has changed
+									scope
+											.$on(
+													"$routeChangeSuccess",
+													function() {
+														var hrefs = [
+																'/#'
+																		+ $location
+																				.path(),
+																'#'
+																		+ $location
+																				.path(), // html5:
+																							// false
+																$location
+																		.path() ]; // html5:
+																					// true
+														angular
+																.forEach(
+																		elem
+																				.find('a'),
+																		function(
+																				a) {
+																			a = angular
+																					.element(a);
+																			if (-1 !== hrefs
+																					.indexOf(a
+																							.attr('href'))) {
+																				a
+																						.parent()
+																						.addClass(
+																								'active');
+																			} else {
+																				a
+																						.parent()
+																						.removeClass(
+																								'active');
+																			}
+																			;
+																		});
+													});
+								}
+							}
+						} ]);
