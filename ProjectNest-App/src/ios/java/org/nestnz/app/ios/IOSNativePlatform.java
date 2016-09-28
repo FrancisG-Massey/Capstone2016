b17b50c093141a064484bfb8e358b1ec7245fbf2
FrancisG-Massey <francisg.massey@gmail.com>
2016-09-28 17:51:02 +1300

    Removed old resources used by the compass

--- ProjectNest-App/src/ios/java/org/nestnz/app/ios/IOSNativePlatform.java
+++ ProjectNest-App/src/ios/java/org/nestnz/app/ios/IOSNativePlatform.java
@@ -16,19 +16,8 @@
  *******************************************************************************/
 package org.nestnz.app.ios;
 
-import org.nestnz.app.services.CompassService;
 import org.nestnz.app.services.NestNativePlatform;
 
 public class IOSNativePlatform extends NestNativePlatform {
 	
-	private IOSCompassService headingService;
-
-	@Override
-	public CompassService getCompassService() {
-		if (headingService == null) {
-			headingService = new IOSCompassService();
-		}
-		return headingService;
-	}
-
 }
