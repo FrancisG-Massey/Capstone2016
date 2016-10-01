07586e68f0e8c1309d9317945c3ec440622e7de3
Sam Hunt <s2112201@ipc.ac.nz>
2016-10-02 00:08:32 +1300

    Issue #68 Make create db schema sql repeatable.

--- ProjectNest-API/db/schema_full.sql
+++ ProjectNest-API/db/schema_full.sql
@@ -19,9 +19,9 @@
 
 */
 
---DROP SCHEMA IF EXISTS public CASCADE;
---CREATE SCHEMA public;
---ALTER SCHEMA public OWNER TO nestnz;
+DROP SCHEMA IF EXISTS public CASCADE;
+CREATE SCHEMA public;
+ALTER SCHEMA public OWNER TO nestnz;
 
 
 -- First create the sequences for the auto-incrememting primary keys
