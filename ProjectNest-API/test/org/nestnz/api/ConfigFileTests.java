26e9945f42e33701772ef37902b784baffff0a8b
Sam Hunt <s2112201@ipc.ac.nz>
2016-10-04 02:43:26 +1300

    Issue #68 Implemented all-datasets param syntax validation test.

--- ProjectNest-API/test/org/nestnz/api/ConfigFileTests.java
+++ ProjectNest-API/test/org/nestnz/api/ConfigFileTests.java
@@ -7,6 +7,7 @@
  **********************************************************/
 package org.nestnz.api;
 
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.google.gson.stream.MalformedJsonException;
@@ -17,8 +18,18 @@ import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Properties;
+import java.util.Set;
+import org.apache.tomcat.jdbc.pool.DataSource;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import org.junit.FixMethodOrder;
@@ -114,13 +125,13 @@ public class ConfigFileTests {
         }
     }
 
-    @Test
     /**
      * Test that any mapped dataset files are parsable JSON strings.
      * @throws IOException if the dataset mappings, or any datasets are unaccessible
      * @throws MalformedJsonException if a dataset cannot be parsed into JSON
      */
-    public void MappedDatasetTargetsAreJSON() throws IOException, MalformedJsonException {
+    @Test
+    public void MappedDatasetsAreJSON() throws IOException, MalformedJsonException {
         Properties prop = new Properties();
         try (InputStream input = new FileInputStream(DATASETS_PATH)) {
             prop.load(input);
@@ -139,5 +150,67 @@ public class ConfigFileTests {
         }
         // All mapped datasets have been resolved by the JSON parser.
         assertTrue(true);
+    }
+    
+    /**
+     * Test that any mapped dataset files have parsable parameter types.
+     * @throws IOException if the dataset mappings, or any datasets are unaccessible
+     * @throws MalformedJsonException if a dataset cannot be parsed into JSON
+     * @throws java.sql.SQLException
+     * @throws java.text.ParseException
+     * @throws java.lang.TypeNotPresentException
+     */
+    @Test
+    public void MappedDatasetsHaveValidParams() throws TypeNotPresentException, IOException, MalformedJsonException, SQLException, ParseException {
+        
+        // Unfortunately we need a db connection to test this (not actually queried, but necessary)
+        String dbConfigPathTest = DBCONFIG_PATH + ".dev".replace("/", SEP);
+        
+        DataSource dsTest = Common.getNestDS(dbConfigPathTest);
+        try (
+            Connection conn = dsTest.getConnection();
+        ) {
+
+            Properties prop = new Properties();
+            try (InputStream input = new FileInputStream(DATASETS_PATH)) {
+                prop.load(input);
+            }
+            for(Entry<Object, Object> e : prop.entrySet()) {
+                File f = new File((SERVLET_CONTEXT_PATH + "/web" + e.getValue().toString().replace("/", SEP)));
+                assertTrue(f.exists());
+
+                // Get the dataset json string from file
+                final String datasetPath = f.toString();
+                byte[] encoded = Files.readAllBytes(Paths.get(datasetPath));
+                final String datasetJSON = new String(encoded, StandardCharsets.UTF_8);
+
+                // Decode from JSON clean enclosing quotes and newlines then return
+                JsonObject jObject = new JsonParser().parse(datasetJSON).getAsJsonObject();
+                Set<Map.Entry<String, JsonElement>> entries = jObject.entrySet();//will return members of your object
+                for (Map.Entry<String, JsonElement> entry: entries) {
+                    String datasetSQL = entry.getValue().getAsString();
+                    
+                    // Remove the returned quotes as the SQL is a string, but only if its not null-length already.
+                    datasetSQL = (datasetSQL.length() > 2) ? datasetSQL.substring(1, datasetSQL.length()-1) : "";
+                    datasetSQL = datasetSQL.replace("\\r\\n", "\n").replace("\\n", "\n");
+                    
+                    // Parse the dataset parameters out of the dataset
+                    Map<String, String> datasetParams = new HashMap<>();
+                    List<String> datasetParamOrder = new ArrayList<>();
+                    Common.parseDatasetParameters(datasetSQL, datasetParams, datasetParamOrder);
+                    
+                    final String cleanSQL = datasetSQL.replaceAll(Common.DATASETPARAM_REGEX, "?");
+                    System.out.println(cleanSQL);
+                    
+                    // An exception will be thrown here if dataset parameters exists with unsupported types
+                    try (PreparedStatement st = conn.prepareStatement(cleanSQL);) {
+                        Common.bindDynamicParameters(st, datasetParams, datasetParamOrder);
+                    }
+                }
+            }
+            // All mapped datasets have been resolved by the JSON parser.
+            assertTrue(true);
+        }
+        Common.closeNestDS();
     }
 }
