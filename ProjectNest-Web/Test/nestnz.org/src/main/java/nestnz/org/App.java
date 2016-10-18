80b79aeac832c4c77939c5b6ade41da949f87708
MJ LEE <itmjlee@gmail.com>
2016-10-18 16:02:06 +1300

    Selenium webdriver + browsers exe files.
    Added a selenium library dependencies using maven

---
+++ ProjectNest-Web/Test/nestnz.org/src/main/java/nestnz/org/App.java
@@ -0,0 +1,13 @@
+package nestnz.org;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
