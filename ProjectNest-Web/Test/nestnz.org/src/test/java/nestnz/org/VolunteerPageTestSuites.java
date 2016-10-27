5f50bb70c00d54ae2df26c24792c60647bc39d6d
MJ LEE <itmjlee@gmail.com>
2016-10-28 03:44:09 +1300

    Added a simple contents testing for volunteer page

---
+++ ProjectNest-Web/Test/nestnz.org/src/test/java/nestnz/org/VolunteerPageTestSuites.java
@@ -0,0 +1,101 @@
+package nestnz.org;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class VolunteerPageTestSuites {
+	  private WebDriver driver;
+	  private String baseUrl;
+	  private boolean acceptNextAlert = true;
+	  private StringBuffer verificationErrors = new StringBuffer();
+
+	  @Before
+	  public void setUp() throws Exception {
+	    baseUrl = "https://www.nestnz.org/";
+	  }
+
+	 /* @Test
+	  public void LaunchTestOnIE() throws Exception {
+		System.setProperty("webdriver.edge.driver",".\\Browsers\\MicrosoftWebDriver.exe");
+		driver = new EdgeDriver();
+		  driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		  testVolunteerPageTestSuites();
+	  }*/
+
+
+	  @Test
+	  public void LaunchTestOnFireFox() throws Exception {
+		  System.setProperty("webdriver.gecko.driver",".\\Browsers\\geckodriver.exe");
+	      driver = new FirefoxDriver();
+	      driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	      testVolunteerPageTestSuites();
+	  }
+	  @Test
+	  public void LaunchTestOnChrome() throws Exception {
+		  System.setProperty("webdriver.chrome.driver", ".\\Browsers\\chromedriver.exe");
+	      driver = new ChromeDriver();
+	      driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	      testVolunteerPageTestSuites();
+	  }
+	  
+  private void testVolunteerPageTestSuites() throws Exception {
+    driver.get("https://www.nestnz.org");
+    driver.findElement(By.linkText("Volunteer")).click();
+    assertEquals("Volunteer", driver.findElement(By.cssSelector("b")).getText());
+    assertTrue(isElementPresent(By.cssSelector("div.jumbotron.ng-scope")));
+    assertEquals("Volunteering with DOC is a rewarding experience.", driver.findElement(By.cssSelector("p")).getText());
+    assertEquals("You are contributing to the health of New Zealand's environment.", driver.findElement(By.xpath("//p[2]")).getText());
+    assertEquals("If you would like to get involved, apply through your local DOC office by following the link below", driver.findElement(By.xpath("//p[3]")).getText());
+    assertTrue(isElementPresent(By.xpath("//a[contains(text(),'Apply via DOC  »')]")));
+    assertEquals("Apply via DOC »", driver.findElement(By.xpath("//a[contains(text(),'Apply via DOC  »')]")).getText());
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
